331d6a81e90ac0bed614c755168d73457f0dbc8b
Oliver Christensen <35222102+orc13a@users.noreply.github.com>
2025-05-18 22:01:18 +0200

    logo added

--- web-dashboard/app/layout.js
+++ web-dashboard/app/layout.js
@@ -3,11 +3,17 @@ import "./globals.css";
 export const metadata = {
     title: "AmbuStatus Dashboard",
     description: "Oversigt over ambulancestatus",
+    icons: {
+        icon: '/favicon.ico',
+    },
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="da">
+            <head>
+                <link rel="icon" href="/favicon.ico" />
+            </head>
             <body>
                 <main>
                     {children}
